fc6fb5e54f35115145739f02f43810aa73f3cdcd
Roarkeyboy <46704594+Roarkeyboy@users.noreply.github.com>
2020-05-11 16:19:28 +0800

    ransac and append working

--- appendimages2.m
+++ appendimages2.m
@@ -7,8 +7,10 @@ function im = appendimages2(image1, image2,scene1,count,matches)
 [rows2, cols2] = size(image2); % image 2 size
 [rows3, cols3] = size(scene1); % scene size
 
-image1 = imresize(image1,[(matches-1)*rows3/matches, (matches-1) * cols1/matches]);
-image2 = imresize(image2,[rows3/matches,(matches-1) * cols1/matches]);
+%image1 = imresize(image1,[(matches-1)*rows3/matches, (matches-1) * cols1/matches]);
+image1 = imresize(image1,[(matches-1)*rows3/matches, cols1]);
+%image2 = imresize(image2,[rows3/matches,(matches-1) * cols1/matches]);
+image2 = imresize(image2,[rows3/matches, cols1]);
 %image1 = imresize(image1,[(count-1)*rows3/count, (matches-1) * cols1/matches]);
 %image2 = imresize(image2,[rows3/count,(matches-1) * cols1/matches]);
 % Now append both images.
--- ransac_match.m
+++ ransac_match.m
@@ -8,7 +8,7 @@
 %
 % Example: match('scene.pgm','book.pgm');
 
-function [match_loc1,match_loc2] = ransac_match(image1, image2,corrPtIdx)
+function [match_loc1,match_loc2,num] = ransac_match(image1, image2,corrPtIdx,display)
 
 % Find SIFT keypoints for each image
 [im1, des1, loc1] = sift(image1);
@@ -45,26 +45,27 @@ for ii = 1:size(des1,1)
         indexer = indexer + 1;        
     end
 end
+if (display)
+    % Create a new image showing the two images side by side.
+    im3 = appendimages(im1,im2);
 
-% Create a new image showing the two images side by side.
-im3 = appendimages(im1,im2);
+    % Show a figure with lines joining the accepted matches.
+    figure('Position', [0 0 size(im3,2)/2 size(im3,1)/2]); % Changed from figure('Position', [100 100 size(im3,2) size(im3,1)]);
+    colormap('gray');
+    imagesc(im3);
+    hold on;
+    cols1 = size(im1,2);
 
-% Show a figure with lines joining the accepted matches.
-figure('Position', [0 0 size(im3,2)/2 size(im3,1)/2]); % Changed from figure('Position', [100 100 size(im3,2) size(im3,1)]);
-colormap('gray');
-imagesc(im3);
-hold on;
-cols1 = size(im1,2);
-
-for i = 1: size(des1,1)
-  if (match(i) > 0)
-    line([loc1(i,2) loc2(match(i),2)+cols1], ...
-         [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
-  end
+    for i = 1: size(des1,1)
+      if (match(i) > 0)
+        line([loc1(i,2) loc2(match(i),2)+cols1], ...
+             [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
+      end
+    end
+    hold off;
 end
-hold off;
 num = sum(match > 0);
-fprintf('Found %d matches.\n', num);
+fprintf('After RANSAC Found %d matches.\n', num);
 
 idx1 = find(match);
 idx2 = match(idx1);
